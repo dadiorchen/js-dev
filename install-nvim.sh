6496658c967aac71457ec698d6d3a2002d952466
dadiorchen <dadiorchen@outlook.com>
2021-03-07 10:39:40 +0800

    feat: script to install: node,nvim,plugin,coc; fail at the place to try to install plugin in nvim

---
+++ install-nvim.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+apt update
+apt install -y git sudo
+mkdir -p ~/work/
+cd ~/work/
+rm -rf neovim
+git clone git@github.com:neovim/neovim.git
+cd neovim
+git fetch --all --tags
+git checkout tags/stable
+git status
+rm -rf build/
+sudo apt-get install -y ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip
+make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/neovim"
+make install
+export PATH="$HOME/neovim/bin:$PATH"
+cat >> ~/.bashrc <<EOF
+export PATH="$HOME/neovim/bin:$PATH"
+EOF
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+source ~/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm
+nvm install 12.16.3
+node -v
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+nvim --headless +PlugInstall +qa
+
+cat >> ~/.bashrc <<EOF
+alias nn="node ~/dadiortemp/openfile/nn.js"
+alias nv="nvim --listen /tmp/nvim"
+EOF
